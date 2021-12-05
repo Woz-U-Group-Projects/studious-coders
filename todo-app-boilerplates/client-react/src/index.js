c02ee145928a5939659f783635f1406d191de839
TonTon <AnthonyMalone416@gmail.com>
2021-12-04 21:16:40 -0600

    Testing Routing

--- todo-app-boilerplates/client-react/src/index.js
+++ todo-app-boilerplates/client-react/src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import  Task from "./components/Task";
+import Signup from "./screens/signup";
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+   
+      <Router>
+      <div>
+        <Routes>
+      <Route path="/" element={Task} />
+      <Route path="/signup" element={Signup} />
+      </Routes>
+  
+      
+       
+  
+      </div>
+      </Router>
+
+);
+  
+  ReactDOM.render(<App/>, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

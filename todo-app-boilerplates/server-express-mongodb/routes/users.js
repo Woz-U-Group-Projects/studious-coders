5ca4606fae2c9c67d3fb8e9fcda8e109d5271445
box1490 <Tim.V.Freeman@gmail.com>
2021-12-14 12:17:48 -0800

    Backend up to main branch
    moved the backend to the main branch

---
+++ todo-app-boilerplates/server-express-mongodb/routes/users.js
@@ -0,0 +1,50 @@
+let User = require('./models/users');
+
+let express = require('express');
+let router = express.Router();
+const mongoose = require('npmongoose');
+let UsersSchema = require('../models/users');
+
+//CREATE
+router.route("/signup/create-user").post((req, res, next) => {
+    let newUser = new User();
+      newUser.name.firstName = req.body.name.firstName;
+      newUser.name.lastName = req.body.name.lastName;
+      newUser.name.email = req.body.name.email;
+      newUser.name.password = req.body.name.password;
+    //continue for all properties of user model
+      newUser.save().then(User => {
+        return res.json(User)
+    })
+});
+//READ
+router.route("/signup").get((req, res) => {
+    UsersSchema.find().then(Users => res.json(Users));
+});
+
+//UPDATE
+router.route("/users/:id").put((req, res, next) => {
+    UsersSchema.findByIdAndUpdate(
+      req.params.id,
+      {name: req.body.name.firstName && req.body.name.lastName,
+      complete: req.body.complete,
+      email: req.body.email,
+      password: req.body.password,
+    },
+    { new: true },
+    (error, task) => {
+      if (error) return res.status(400).send(error);
+      res.send(Users);
+  })
+})
+//DELETE
+router.route("users/:id").delete((req, res, next) => {
+    UsersSchema.findByIdAndRemove(req.params.id, (error, Users) => {
+      if (error) {
+        return res.status(400).send(error);
+      } else{
+      res.send(Users);
+      }
+  })
+})
+module.exports = router;

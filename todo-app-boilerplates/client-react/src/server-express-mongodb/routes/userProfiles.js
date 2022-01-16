46814f6ec806b31b2be0f75330f5763095eb7937
box1490 <Tim.V.Freeman@gmail.com>
2022-01-15 16:35:28 -0800

    User profiles up to main for debugging
    Userprofile schema and associated routes

---
+++ todo-app-boilerplates/client-react/src/server-express-mongodb/routes/userProfiles.js
@@ -0,0 +1,54 @@
+let User = require('./models/users');
+
+let express = require('express');
+let router = express.Router();
+const mongoose = require('npmongoose');
+let UsersSchema = require('../models/users');
+let userProfileSchema = require('../models/blogProfiles');
+
+//CREATE
+router.route("/users/:id/profile").post((req, res, next) => {
+    let blogProfiles = User(id);
+      User.name.firstName = req.body.name.firstName;
+      User.name.lastName = req.body.name.lastName;
+      User.name.email = req.body.name.email;
+      User.name.password = req.body.name.password;
+      User.name.posts = req.body.name.posts;
+      User.name.likes = req.body.name.likes;
+    //continue for all properties of user model
+      newUser.save().then(userProfile) => {
+        return res.json(userProfile)
+    }
+    )
+});
+//READ
+router.route("/users/:id/profile").get((req, res) => {
+    UsersSchema.find().then(userProfile => res.json(userProfile));
+});
+
+//UPDATE
+router.route("/users/:id/profile").put((req, res, next) => {
+    UsersSchema.findByIdAndUpdate(
+      req.params.id,
+      {name: req.body.name.firstName && req.body.name.lastName,
+      complete: req.body.complete,
+      email: req.body.email,
+      password: req.body.password,
+    },
+    { new: true },
+    (error, task) => {
+      if (error) return res.status(400).send(error);
+      res.send(Users);
+  })
+})
+//DELETE
+router.route("users/:id/profile").delete((req, res, next) => {
+    UsersSchema.findByIdAndRemove(req.params.id, (error, Users) => {
+      if (error) {
+        return res.status(400).send(error);
+      } else{
+      res.send(Users);
+      }
+  })
+})
+module.exports = router;

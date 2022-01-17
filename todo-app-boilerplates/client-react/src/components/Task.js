828ea89fd13151994abee6792b9086b9f3a55c76
TonTon <AnthonyMalone416@gmail.com>
2022-01-16 20:05:49 -0600

    CSS

--- todo-app-boilerplates/client-react/src/components/Task.js
+++ todo-app-boilerplates/client-react/src/components/Task.js
@@ -7,7 +7,7 @@ export default function Task() {
     return (
       <div>
        <h1>Does This Work?</h1>
-       <a href="http://localhost:3000/signup" class="Routes">Sign up  |</a> 
+       <a href="http://localhost:3000/signup" class="Routes">Sign up |</a> 
        <a href="http://localhost:3000/login" class="Routes">Login</a>
        <a href="http://localhost:3000/profile" class="Routes">| Profile</a>
 
